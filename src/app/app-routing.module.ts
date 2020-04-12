d9d0528a1dfd18bc80293739395e8c4dff5ce2fb
root <root@serverFrontend.lan>
2020-04-12 14:53:41 -0500

    Firmas Front End

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {FormComponent} from './form/form.component';
+import {LoginComponent} from './login/login.component';
+import {HomeSessionComponent} from './home-session/home-session.component';
+import {UploadComponent} from './upload/upload.component';
+import {ValidarDocumentsComponent} from './validar-documents/validar-documents.component';
 
 
-const routes: Routes = [];
+const routes: Routes =[
+  {path: 'login', component: LoginComponent},
+  {path: 'registro', component: FormComponent},
+  {path: 'home', component: HomeSessionComponent},
+  {path: 'upload', component:ValidarDocumentsComponent},
+  {path: '', redirectTo: 'login', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import {HttpClientModule} from '@angular/common/http';
+import { FormComponent } from './form/form.component';
+import { LoginComponent } from './login/login.component';
+import { HomeSessionComponent } from './home-session/home-session.component';
+import {NameCompletePipe} from "./home-session/name-complete.pipe";
+import { UploadComponent } from './upload/upload.component';
+import { ValidarDocumentsComponent } from './validar-documents/validar-documents.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    FormComponent,
+    LoginComponent,
+    HomeSessionComponent,
+    NameCompletePipe,
+    UploadComponent,
+    ValidarDocumentsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
